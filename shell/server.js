cb52b7ac14cdeb0cf6b3034183c56258c88e3f76
Alexis <a.regnaud@samsumg.com>
2021-08-19 17:13:16 -0400

    Fix server production / add heroku build

--- shell/server.js
+++ shell/server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT;
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('./client/build'));
+  app.use(express.static('./client/dist'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 
