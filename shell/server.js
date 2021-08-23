3d1d11451a389112a68f50d925b0617bf5ccbded
Alexis <a.regnaud@samsumg.com>
2021-08-23 15:39:09 -0400

    Try server proxy

--- shell/server.js
+++ shell/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(createProxyMiddleware('/api/ssr-2', { target: process.env.APP2_URL }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('./client/dist'));
